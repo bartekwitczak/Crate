93dc30e36c3bb83897914e284c71f1442860f541
Bartlomiej Witczak <witczakbartlomiej@gmail.com>
2016-04-12 23:17:44 +0200

    Setup environment for TDD with Carthage, Quick & Nimble

---
+++ CrateTests/CrateSpec.swift
@@ -0,0 +1,12 @@
+import Quick
+import Nimble
+
+class CrateSpec: QuickSpec {
+    
+    override func spec() {
+        it("should setup correctly") {
+            expect(true).to(beTrue())
+        }
+    }
+    
+}
